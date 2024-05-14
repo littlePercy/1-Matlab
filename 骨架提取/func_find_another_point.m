68bf9c886c2727a994ca93fc71b11b6e890c7a25
littlePercy <52816016+littlePercy@users.noreply.github.com>
2024-05-14 17:40:18 +0800

    Update func_find_another_point.m

--- 骨架提取/func_find_another_point.m
+++ 骨架提取/func_find_another_point.m
@@ -1,1 +1,20 @@
+function nextPoint = func_find_another_point(indexPoint,indexList,pointNum)
+% indexList = cell_1{2, clusPoints(j, 3)};
+% indexPoint = clusPoints(j,1:2);
+[~, loc] = ismember(indexPoint, indexList, 'rows');                % 找出端点在血管段的位置，首/尾
+if loc==1
+    if length(indexList)>pointNum
+        nextPoint = indexList(pointNum, :);
+    else
+        nextPoint = indexList(end, :);
+    end
+else
+    if length(indexList)>pointNum
+        nextPoint = indexList(end-pointNum+1, :);
+    else
+        nextPoint = indexList(1, :);
+    end
+end
+
+
 
